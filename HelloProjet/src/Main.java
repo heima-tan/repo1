43a569b69d442083aa56e003cefc42c4b733b8c9
tan <1165665339@qq.com>
2019-06-24 19:13:55 +0800

    dev第三次修改java.main文件

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("第一步,改变自己");
 		System.out.println("第二步,继续改变自己");
+		System.out.println("第三步,我要改变世界");
     }
 }
